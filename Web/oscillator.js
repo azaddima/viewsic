8e176758933f64c2da70aed8f8c4e8ea35e125e6
pascalrutkowski <patgun@hotmail.de>
2019-11-05 09:51:51 +0100

    Refactor from var to let in oscillator.js

--- Web/oscillator.js
+++ Web/oscillator.js
@@ -9,11 +9,11 @@ let customWaveform = null;
 let sineTerms = null;
 let cosineTerms = null;*/
 
-var cmajor = [32.703195662574829, 36.708095989675945, 41.203444614108741,
-              43.653528929125485, 48.999429497718661, 55.000000000000000,
-              61.735412657015513, 32.703195662574829 * 2];
+let cmajor = [32.703195662574829, 36.708095989675945, 41.203444614108741,
+    43.653528929125485, 48.999429497718661, 55.000000000000000,
+    61.735412657015513, 32.703195662574829 * 2];
 
-var context = new AudioContext(),
+let context = new AudioContext(),
     oscillator,
     isPlaying = false;
     gainNode = context.createGain(),
@@ -24,16 +24,16 @@ var context = new AudioContext(),
 gainNode.connect(context.destination);
 
 slider.oninput = function() {
-  var gainValue = (this.value / 100),
-      viewValue = gainValue * 100;
-  output.innerHTML = viewValue + "";
-  gainNode.gain.value = gainValue;
+    let gainValue = (this.value / 100),
+        viewValue = gainValue * 100;
+        output.innerHTML = viewValue + "";
+        gainNode.gain.value = gainValue;
 };
 
 function playSound() {
-    if(isPlaying === false) {
-        var e = document.getElementById("waveform");
-        var type = e.options[e.selectedIndex].text;
+    if(!isPlaying) {
+        let e = document.getElementById("waveform");
+        let type = e.options[e.selectedIndex].text;
         oscillator = context.createOscillator();
         oscillator.connect(gainNode);
         oscillator.type = type;
@@ -46,28 +46,28 @@ function playSound() {
 
 function stopSound() {
     if(oscillator){
-    oscillator.stop(context.currentTime);
-    oscillator.disconnect();
-    isPlaying = false;
+        oscillator.stop(context.currentTime);
+        oscillator.disconnect();
+        isPlaying = false;
     }
 }
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("myDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
+    if (!event.target.matches('.dropbtn')) {
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
-  }
 };
