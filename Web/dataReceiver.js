4bc15ae6804509ad468417949b83beb3eb9d1096
pascalrutkowski <patgun@hotmail.de>
2019-11-12 13:59:43 +0100

    Fix issues in dataReciever.js

--- Web/dataReceiver.js
+++ Web/dataReceiver.js
@@ -3,7 +3,7 @@ var socket = new WebSocket("ws://127.0.0.1:8765/");
 
 socket.onmessage = function (event) {
    let data = event.data;
-   freqValueContour = parseInt(data) * 10
+   freqValueContour = parseInt(data) * 10;
 
    if(true){
          console.log("Data received: " + freqValueContour);
