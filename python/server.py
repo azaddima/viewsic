1438d3a6073653a96e5c7e045422f2045f016d9f
pascalrutkowski <patgun@hotmail.de>
2019-11-12 14:02:53 +0100

    Cleanup code in server.py

--- python/server.py
+++ python/server.py
@@ -14,16 +14,18 @@ async def video_data(websocket, path):
 # Event Loop erstellen, da der Server in einem anderem Thread laufen soll
 loop = asyncio.new_event_loop()
 
+
 # Methode zum Starten des Servers mit erstelltem Event Loop
 def start_server(loop):
-   asyncio.set_event_loop(loop)
-   run_server = websockets.serve(video_data, "127.0.0.1", 8765)
-   asyncio.get_event_loop().run_until_complete(run_server)
-   asyncio.get_event_loop().run_forever()
+    asyncio.set_event_loop(loop)
+    run_server = websockets.serve(video_data, "127.0.0.1", 8765)
+    asyncio.get_event_loop().run_until_complete(run_server)
+    asyncio.get_event_loop().run_forever()
+
 
 # Starten des Servers in einem Thread
 # Ein Daemon ist ein Thread, der sich selbst beendet, falls das Programm endet
-serverThread = threading.Thread(target=start_server, args=[loop], daemon= True)
+serverThread = threading.Thread(target=start_server, args=[loop], daemon=True)
 serverThread.start()
 
 # Start programm logic
