0d0bb38465fd78c38ed304fa5a402bb8592eed0d
pascalrutkowski <patgun@hotmail.de>
2019-12-04 18:55:27 +0100

    Implement minor scale and possible change logic (not able to test ist yet)

--- Web/SoundManager.js
+++ Web/SoundManager.js
@@ -1,15 +1,24 @@
 //#document.write('<script type="text/javascript" src="dataReceiver.js"></script>');
 let pentatonicMinor = [0, 3, 5, 7, 10, 12];
 let pentatonicMajor = [0, 2, 4, 7, 9, 12];
-let majorScale2 = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23 ]
+let majorScale = [0, 2, 4, 5, 7, 9, 11, 12, 14, 16, 17, 19, 21, 23 ];
+let minorScale = [0, 2, 3, 5 , 7, 8, 10, 11, 12];
 
 let activeScale = [];
+let scaleSelect = minorScale;
+
+function setScaleType() {
+        scaleSelect = document.getElementById("scales");
+        console.log('scale changed to: ' + scaleSelect);
+}
+
 // todo - put this into events from buttonchanges!
 function initSound(){
     //todo - remove after color wheel is implemented!
     let keyTone = 5;
+    setScaleType();
     // default is major scale
-    activeScale = createScale(keyTone, 3,  pentatonicMinor );
+    activeScale = createScale(keyTone, 3,  scaleSelect);
 }
 
 initSound();
