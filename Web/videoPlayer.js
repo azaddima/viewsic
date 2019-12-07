b2baf1a28a4207e8513aa0a8dae6187b97e33c92
pascalrutkowski <patgun@hotmail.de>
2019-12-07 13:06:13 +0100

    Implement video player with play and pause function

---
+++ Web/videoPlayer.js
@@ -0,0 +1,11 @@
+video = document.getElementById("video");
+
+document.getElementById("playbtn").onclick = function () {
+
+    video.play();
+};
+
+document.getElementById("stopbtn").onclick = function () {
+
+    video.pause();
+};
