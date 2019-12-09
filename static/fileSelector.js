fb0c6f62a5537bfc82e342e46ec7f404635b2915
pascalrutkowski <patgun@hotmail.de>
2019-12-09 16:08:03 +0100

    Refactor experimental fileSelector.js

--- static/fileSelector.js
+++ static/fileSelector.js
@@ -12,16 +12,6 @@ $('#selectDialog').change(function(){
 // Create new FileReader as a variable
 let reader = new FileReader();
 
-function _arrayBufferToBase64( buffer ) {
-    var binary = '';
-    var bytes = new Uint8Array( buffer );
-    var len = bytes.byteLength;
-    for (var i = 0; i < len; i++) {
-        binary += String.fromCharCode( bytes[ i ] );
-    }
-    return window.btoa( binary );
-}
-
 // Onload Function will run after video has loaded
 reader.onload = function(file){
 let fileContent = file.target.result;
@@ -30,10 +20,16 @@ let index = fileContent.search(',');
 let byteArray = fileContent.slice(index + 1);
 console.log(byteArray);
 sendMessage("videoFile", byteArray);
+upload(byteArray, "myVideo.mp4");
 };
 
 // Get the selected video from Dialog
 reader.readAsDataURL(this.files[0]);
+
+function upload(text, name, type) {
+  let fileWriter = new File(text, name);
+  //let file = new Blob([text], {type: type});
+}
 
 });
 
