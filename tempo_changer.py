22e0080cef2a26e1f9fb61683104bc7d2163c07c
Azad Amid <azad.amid@haw-hamburg.de>
2019-12-10 01:32:33 +0100

    Add tempo changer based on contours

--- tempo_changer.py
+++ tempo_changer.py
@@ -1,14 +1,32 @@
 # from python import websocket_server
+import math
+
 # todo - change variable access
 message_interval = 0.5
+
 
 def update(data):
     change_global_tempo(data)
 
+
 def change_global_tempo(bpm):
-    global  message_interval
+    global message_interval
 
     sec = 1 / (bpm / 60)
     message_interval = sec
     print('intervall changed to ' + str(sec))
 
+
+def tempo_contour(value):
+    global message_interval
+
+    # if value > 1300:
+    bpm = math.log10(2) * value
+    if bpm < 60:
+        bpm = 60
+
+    if bpm > 450:
+        bpm = 450
+
+    sec = 1 / (bpm / 60)
+    message_interval = sec
